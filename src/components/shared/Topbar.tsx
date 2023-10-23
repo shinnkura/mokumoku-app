70db1ea119b76cff543594860529a0835f6bf898
shinnkura <n.shinzo1010@icloud.com>
2023-10-23 23:49:20 +0900

    fix(Topbar): Topbarのタイトルを変更

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -9,7 +9,9 @@ function Topbar() {
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
         <Image src="/assets/logo.png" alt="logo" width={28} height={28} />
-        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">
+          MokuMoku
+        </p>
       </Link>
 
       <div className="flex items-center gap-1">
