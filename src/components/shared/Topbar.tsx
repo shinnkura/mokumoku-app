50398b28e87635cb3daa5a5d7e7298e72ca4c59d
shinnkura <n.shinzo1010@icloud.com>
2023-10-24 12:20:19 +0900

    feat(RightSidebar): RightSidebarを追加

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -3,6 +3,7 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from "@clerk/themes";
 
 function Topbar() {
   return (
@@ -32,6 +33,7 @@ function Topbar() {
 
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
